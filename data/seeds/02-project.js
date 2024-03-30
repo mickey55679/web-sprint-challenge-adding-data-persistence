9d9d516f3149586cfa4338665e0815d97bbb42a7
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-30 16:38:02 -0500

    wip

--- data/seeds/02-project.js
+++ data/seeds/02-project.js
@@ -1,3 +1,17 @@
-exports.seed = function (knex) {
-   
-}
+// seeds/02_projects.js
+
+exports.seed = async function (knex) {
+  // Inserts seed entries
+  await knex("projects").insert([
+    {
+      project_name: "Build a garden shed",
+      project_description: "Construct a small garden shed.",
+      project_completed: false,
+    },
+    {
+      project_name: "Develop a web application",
+      project_description: "Create a full-stack web application.",
+      project_completed: false,
+    },
+  ]);
+};
